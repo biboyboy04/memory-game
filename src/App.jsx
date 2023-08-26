c2a253fa1ac494e02e246a308521deeaaf17a44d
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-08-27 03:19:51 +0800

    Update monster.js and Add fixed random card render
    Put clicked property on monster.js to track which monster is clicked.
    
    Fixed number random card render to prepare for different game difficulties.
    
    Add card change bg on hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import Card from "./components/Card.jsx";
-import monsters from "./monsters.js";
+import monstersData from "./monsters.js";
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const numberOfMonsters = 3; // Set the desired number of monsters
+
+  useEffect(() => {
+    const randomMonsters = [];
+    const availableMonsters = [...monstersData]; // Create a copy of monstersData
+
+    while (
+      randomMonsters.length < numberOfMonsters &&
+      availableMonsters.length > 0
+    ) {
+      // Get a random index from availableMonsters
+      const randomIndex = Math.floor(Math.random() * availableMonsters.length);
+
+      // Remove the monster at the random index from availableMonsters
+      //and add it to randomMonsters
+      randomMonsters.push(availableMonsters.splice(randomIndex, 1)[0]);
+    }
+
+    setMonsters(randomMonsters);
+  }, [numberOfMonsters]);
+
   return (
-    <>
-      <div className="card-container">
-        {monsters.map((monster) => {
-          return (
-            <Card
-              key={monster.name}
-              name={monster.name}
-              src={"../../public/monsters/" + monster.imageUrl}
-            />
-          );
-        })}
-      </div>
-    </>
+    <div className="card-container">
+      {monsters.map((monster) => (
+        <Card
+          key={monster.name}
+          name={monster.name}
+          src={`/monsters/${monster.imageUrl}`}
+        />
+      ))}
+    </div>
   );
 }
+
 export default App;
--- src/monsters.js
+++ src/monsters.js
@@ -1,17 +1,17 @@
-const monsters = [
-  { name: "Anjanath", imageUrl: "Anjanath.png" },
-  { name: "Barroth", imageUrl: "Barroth.png" },
-  { name: "Diablos", imageUrl: "Diablos.png" },
-  { name: "Great Girros", imageUrl: "Great_Girros.png" },
-  { name: "Great Jagras", imageUrl: "Great_Jagras.png" },
-  { name: "Jyuratodus", imageUrl: "Jyuratodus.png" },
-  { name: "Kulu-Ya-Ku", imageUrl: "Kulu-Ya-Ku.png" },
-  { name: "Legiana", imageUrl: "Legiana.png" },
-  { name: "Paolumu", imageUrl: "Paolumu.png" },
-  { name: "Pukei-Pukei", imageUrl: "Pukei-Pukei.png" },
-  { name: "Rathalos", imageUrl: "Rathalos.png" },
-  { name: "Rathian", imageUrl: "Rathian.png" },
-  { name: "Tobi-Kadachi", imageUrl: "Tobi-Kadachi.png" },
+const monstersData = [
+  { name: "Anjanath", imageUrl: "Anjanath.png", clicked: false },
+  { name: "Barroth", imageUrl: "Barroth.png", clicked: false },
+  { name: "Diablos", imageUrl: "Diablos.png", clicked: false },
+  { name: "Great Girros", imageUrl: "Great_Girros.png", clicked: false },
+  { name: "Great Jagras", imageUrl: "Great_Jagras.png", clicked: false },
+  { name: "Jyuratodus", imageUrl: "Jyuratodus.png", clicked: false },
+  { name: "Kulu-Ya-Ku", imageUrl: "Kulu-Ya-Ku.png", clicked: false },
+  { name: "Legiana", imageUrl: "Legiana.png", clicked: false },
+  { name: "Paolumu", imageUrl: "Paolumu.png", clicked: false },
+  { name: "Pukei-Pukei", imageUrl: "Pukei-Pukei.png", clicked: false },
+  { name: "Rathalos", imageUrl: "Rathalos.png", clicked: false },
+  { name: "Rathian", imageUrl: "Rathian.png", clicked: false },
+  { name: "Tobi-Kadachi", imageUrl: "Tobi-Kadachi.png", clicked: false },
 ];
 
-export default monsters;
+export default monstersData;
